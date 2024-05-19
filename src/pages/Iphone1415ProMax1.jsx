84cdafff524596952969950a1726707a23459dd5
satish-hebbal <satihhebbal96@gmail.com>
2024-05-19 19:39:21 +0530

    3rd cmt

--- src/pages/Iphone1415ProMax1.jsx
+++ src/pages/Iphone1415ProMax1.jsx
@@ -4,8 +4,9 @@ export default function Iphone1415ProMax1() {
       <div className="shadow-[0px_0px_2px_0px_rgba(23,26,31,0.12),0px_0px_1px_0px_rgba(23,26,31,0.07)] rounded-[4px] bg-[#3170BF] relative m-[0_0_12px_0] p-[7px_36.4px_3px_15px] w-[100%] box-sizing-border">
         <div className="relative flex flex-row justify-between w-[100%] h-[fit-content] box-sizing-border">
           <div className="flex flex-row box-sizing-border">
-            <div className="bg-[url('assets/images/Image1.png')] bg-[50%_50%] bg-cover bg-no-repeat m-[0_7px_2px_0] w-[20px] h-[20px]">
-            </div>
+            {/* <div className="bg-[url('src\pages\Image1.png')] bg-[50%_50%] bg-cover bg-no-repeat m-[0_7px_2px_0] w-[20px] h-[20px]">
+            </div> */}
+            <img src="src\pages\Image1.png" className="bg-[50%_50%] bg-cover bg-no-repeat m-[0_7px_2px_0] w-[20px] h-[20px]" alt="" />
             <div className="m-[10px_0_0_0] inline-block break-words font-['Inter'] font-normal text-[7px] leading-[1.714] text-[#FFFFFF]">
             ONLINE CERTIFICATION
             </div>
@@ -14,7 +15,7 @@ export default function Iphone1415ProMax1() {
             <div className="m-[2px_15.3px_2px_0] inline-block break-words font-['Inter'] font-normal text-[6px] leading-[1.667] text-[#FFFFFF]">
             Home
             </div>
-            <div className="m-[1px_17.9px_1px_0] inline-block break-words font-['Inter'] font-normal text-[7px] leading-[1.714] text-[#FFFFFF]">
+            <div className="m-[1px_17.9px_1px_0] inline-block break-words font-['Inter'] font-normal text -[7px] leading-[1.714] text-[#FFFFFF]">
             Semester Information
             </div>
             <span className="m-[0_10.8px_0_0] break-words font-['Inter'] font-normal text-[8px] leading-[1.75] text-[#FFFFFF]">
@@ -41,8 +42,7 @@ export default function Iphone1415ProMax1() {
           </span>
         </div>
         <div className="m-[0_0_34px_0] flex flex-row self-start w-[fit-content] box-sizing-border">
-          <h1>this is image</h1>
-          <img src="assets\images\Image28.png" className="h-20" alt="" />
+          <img src="src\pages\Image28.png" className="h-20" alt="" />
           <div className="m-[6px_0_0_0] flex flex-col box-sizing-border">
             <div className="m-[0_13px_16px_13px] inline-block self-start break-words font-['Inter'] font-normal text-[7px] leading-[1.571] text-[#171A1F]">
             ALRAQAB<br />
